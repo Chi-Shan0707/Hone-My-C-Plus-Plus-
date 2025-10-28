e345014f76a96aef0ee51cc3f7d0fc3e860ceb0c
juruocoding <masterwuguicyh@163.com>
2025-10-28 18:03:57 +0800

    2025/10/28 ctrl shift p   terminal select default 挑选cmd？？？为什么c运行得了 c++运行不了 /* 1. 检查 launch.json 配置 确保您的 .vscode/launch.json 配置正确：
    externalconsole true与false的区别
    */
    
    /*
    bash终端里ERROR: Unable to start debugging. Unexpected GDB output from command "-exec-run". During startup program exited with code 0xc0000139.
    The program 'D:\C_C++\Hone_My_C++\Chi's Binary_Search_Tree\fhq_Treap.exe' has exited with code 0 (0x00000000). 已知是cppdbg模式，但c程序能正确运行；在换成bash终端前，c++也能正确运行；我的程序#include<iostream>
    int main()
    {
        int d;
        std::cin>>d;
        return 0;
    }简单如这样也会直接崩溃，虽然bash那边是蓝色的显示编译成功
    
    2. 切换终端类型
    在 VS Code 中尝试：
    
    按 Ctrl+Shift+P
    
    输入 "Terminal: Select Default Profile"
    
    选择 "Command Prompt" 而不是 "Git Bash"
    
    重启终端和 VS Code
    */

--- Chi's sort/quick_sort.h
+++ Chi's sort/quick_sort.h
@@ -17,23 +17,45 @@ class Sort
             int pivot=partition(a,l,r);
             quick_sort(a,l,pivot-1);
             quick_sort(a,pivot+1,r);
-            /*
-            for(int i=1;i<=n;++i)
-            {
-                cout<<a[i]<<" ";
-            }
-            cout<<endl<<"-------------------"<<endl;
-            */
         }
     private:
+        void swap(T &u,T &v)
+        {
+            T temp=u;
+            u=v;
+            v=temp;
+        }
         int partition(T a[],int l,int r)
+        {
+            int left=l,right=r;
+            T key=a[left];
+            while(left<right)
+            {
+                //先考虑右侧≥key
+                while(right>left&&a[right]>=key)--right;
+                swap(a[left],a[right]);
+
+                //再考虑左侧≤key
+                while(left<right&&a[left]<=key)++left;
+                swap(a[left],a[right]);
+        /*
+        很精妙的算法，当在第一个while的时候，key在left上，当right指向一个比较小的数的时候，就会互调leftright，开启第二个while，此时key在right上，移动left看left侧的值有没有≤key的，如果有，则再次与right交换，使得这个较大的值变到key右侧
+        */
+            }
+            return right;//此时left==right
+        }
+};
+
+
+/*
+int partition(int a[],int l,int r)
         {
             int left=l,right=r;
             while(left<right)
             {
                 while(right>left&&a[right]>=a[l])--right;
                 while(left<right&&a[left]<=a[l])++left;
-/*
+
 注意点：
 两个while顺序不能换&&至少有一个得有取等
 和left不能设置成l+1
@@ -41,12 +63,14 @@ class Sort
 当l=r+1的时候，如果left=l+1,大的while就不执行了
 当l=r+1的时候，如果++left在前，那么就会直接left=right=r
 指针需要再细细品
-*/
+
                 swap(a[left],a[right]);
             }
             swap(a[l],a[right]);
             return right;
         }
-};
+
+*/
 
 #endif
+
--- Chi's sort/use_my_quick_sort.cpp
+++ Chi's sort/use_my_quick_sort.cpp
@@ -4,11 +4,48 @@ using namespace std;
 int main()
 {
     int n;
+    cout << "请输入元素个数: ";
+    cout.flush();
     cin>>n;
     int a[1002];
+    cout << "请输入" << n << "个数字: ";
+    cout.flush();
     for(int i=1;i<=n;i++)cin>>a[i];
     Sort<int> my_quick_sort;
     my_quick_sort.quick_sort(a,1,n);
+    cout << "排序结果: ";
     for (int  i=1;i<=n;++i)cout<<a[i]<<" ";
+    cout << endl;
     return 0;
 }
+
+/*
+1. 检查 launch.json 配置
+确保您的 .vscode/launch.json 配置正确：
+
+
+externalconsole true与false的区别
+*/
+
+/*
+bash终端里ERROR: Unable to start debugging. Unexpected GDB output from command "-exec-run". During startup program exited with code 0xc0000139.
+The program 'D:\C_C++\Hone_My_C++\Chi's Binary_Search_Tree\fhq_Treap.exe' has exited with code 0 (0x00000000). 已知是cppdbg模式，但c程序能正确运行；在换成bash终端前，c++也能正确运行；我的程序#include<iostream>
+int main()
+{
+    int d;
+    std::cin>>d;
+    return 0;
+}简单如这样也会直接崩溃，虽然bash那边是蓝色的显示编译成功
+
+
+2. 切换终端类型
+在 VS Code 中尝试：
+
+按 Ctrl+Shift+P
+
+输入 "Terminal: Select Default Profile"
+
+选择 "Command Prompt" 而不是 "Git Bash"
+
+重启终端和 VS Code
+*/
